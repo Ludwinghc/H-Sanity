e80219f73db8890cad556791d664b06e38e85ba7
David Bolaños <dabog0106@gmail.com>
2023-09-17 17:54:14 -0500

    Media root establishment config

--- HSanity/establishment/urls.py
+++ HSanity/establishment/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('hotel/', views.home, name='home'),
     path('hotel/view', views.view, name='view'),
     path('hotel/create', views.create, name='create'),
     path('hotel/edit', views.edit, name='edit'),
-]
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
