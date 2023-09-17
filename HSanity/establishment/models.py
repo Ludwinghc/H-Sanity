5e46887f963531c51a683d3c56b0d166435a9a72
David Bolaños <dabog0106@gmail.com>
2023-09-17 13:26:30 -0500

    Stablishment draft model

--- HSanity/establishment/models.py
+++ HSanity/establishment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Establishment():
+    id = models.AutoField(primary_key=True),
+    name = models.CharField(max_length=100),
+    image = models.ImageField(null=True, upload_to=-'/images'),
+    address = models.CharField(max_length=50),
+    contact = models.BigIntegerField(),
+    mail = models.CharField(max_length=50),
+    website = models.CharField(max_length=100),
+    rut = models.BigIntegerField(),
