27c10069571b697305fe3776899b42b090219087
David Bolaños <dabog0106@gmail.com>
2023-09-17 17:30:22 -0500

    Migration model and admin config

--- HSanity/establishment/models.py
+++ HSanity/establishment/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Establishment():
-    id = models.AutoField(primary_key=True),
-    name = models.CharField(max_length=100),
-    image = models.ImageField(null=True, upload_to=-'/images'),
-    address = models.CharField(max_length=50),
-    contact = models.BigIntegerField(),
-    mail = models.CharField(max_length=50),
-    website = models.CharField(max_length=100),
-    rut = models.BigIntegerField(),
+class Establishment(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name='Nombre',max_length=100, null=True)
+    photo = models.ImageField(verbose_name='Foto del Establecimiento', upload_to='images/hotelProfile', null=True)
+    address = models.CharField(verbose_name='Direccion', max_length=50, null=True)
+    contact = models.BigIntegerField(verbose_name='Contacto', null=True)
+    mail = models.CharField(verbose_name='Correo', max_length=50, null=True)
+    website = models.CharField(verbose_name='Pagina Web', max_length=100, null=True)
+    rut = models.BigIntegerField(verbose_name='RUT', null=True)
+
+    def __str__(self):
+        row = str(self.id) + "-" + self.name
+        return row
+    
+    def delete(self, using=None, keep_perents=False):
+        self.photo.storage.delete(self.photo.name)
+        super().delete()
--- HSanity/establishment/views.py
+++ HSanity/establishment/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-
+from .models import Establishment
 # Create your views here.
 
 def home(request):
     return render(request, 'general/home.html')
 
 def view(request):
-    return render(request, 'hotel/view.html')
+    hoteles = Establishment.objects.all()
+    return render(request, 'hotel/view.html', {'hoteles' : hoteles})
 
 def create(request):
     return render(request, 'hotel/create.html')
