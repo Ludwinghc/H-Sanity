ce562e13ad03d65ac8f81f07f4c771937200462c
Ludwing Hernandez Caro <79872566+Ludwinghc@users.noreply.github.com>
2023-09-25 10:29:59 -0500

    update url name in the auditor

--- HSanity/auditor/urls.py
+++ HSanity/auditor/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('auditor/', views.inicioAuditor, name='inicioAuditor'),
-    path('auditor/view', views.viewAuditor, name='viewAuditor'),
-    path('auditor/create', views.create, name='create'),
-    path('auditor/edit', views.edit, name='edit'),
-    path('auditor/edit/<int:id>', views.edit, name='edit'),
+    path('auditor/view-Auditor', views.viewAuditor, name='viewAuditor'),
+    path('auditor/create-Auditor', views.create, name='createAuditor'),
+    path('auditor/edit-Auditor', views.edit, name='editAuditor'),
+    path('auditor/edit-Auditor/<int:id>', views.edit, name='editAuditor'),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
