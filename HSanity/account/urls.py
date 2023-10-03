fa480764ff1fa7539d278a928228c687b0bae2b9
David Bolaños <dabog0106@gmail.com>
2023-10-03 14:40:19 -0500

    Basic user login and register completed

--- HSanity/account/urls.py
+++ HSanity/account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logOutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
     path('', views.home, name='auditorHome'),
     path('auditor/', views.viewAuditor, name='viewAuditor'),
--- HSanity/account/views.py
+++ HSanity/account/views.py
@@ -3,37 +3,62 @@ from django.http import HttpResponse
 from .models import Auditor
 from .forms import AuditorForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
+
 def loginPage(request):
-    return render(request, 'account/general/login.html')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+    
+        if user is not None:
+            login(request, user)
+            return redirect('auditorHome')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+    return render(request, "account/general/login.html", {})
+
+def logOutUser(request):
+    logout(request)
+    return redirect('login')
 
 def registerPage(request):
     form = CreateUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
-    return render(request, 'account/general/register.html', {'form': form})
+            return redirect("login")
+    return render(request, "account/general/register.html", {"form": form})
+
 
 def home(request):
-    return render(request, 'account/auditor/dashboard.html')
+    return render(request, "account/auditor/dashboard.html")
+
 
 def createAuditor(request):
     form_auditor = AuditorForm(request.POST or None)
     if form_auditor.is_valid():
         form_auditor.save()
-        return redirect('inicioAuditor')
-    return render(request, 'account/auditor/createAuditor.html', {
-        'form_auditor' : form_auditor})
+        return redirect("inicioAuditor")
+    return render(
+        request, "account/auditor/createAuditor.html", {"form_auditor": form_auditor}
+    )
+
 
 def viewAuditor(request):
-    auditores = Auditor.objects.get(id = 1)
-    form_auditor = AuditorForm(request.POST or None, instance = auditores)
+    auditores = Auditor.objects.get(id=1)
+    form_auditor = AuditorForm(request.POST or None, instance=auditores)
     if form_auditor.is_valid() and request.POST:
         form_auditor.save()
-        return redirect('inicioAuditor')
-    return render(request, 'account/auditor/viewAuditor.html', {
-        'auditor' : auditores, 'form_auditor' : form_auditor})
+        return redirect("inicioAuditor")
+    return render(
+        request,
+        "account/auditor/viewAuditor.html",
+        {"auditor": auditores, "form_auditor": form_auditor},
+    )
